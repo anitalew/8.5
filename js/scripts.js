c6546e92005713aab9b9d9b4dc0763c912c0af9d
Anita Lew Kiedrowska <anitalk@poczta.onet.pl>
2018-05-15 11:27:58 +0200

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 7,
+var	a = 7,
 	h = 3,
-	triangleArea = a*h/2;
+	triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-prompt(triangleArea=a*h/2)
+prompt(triangleArea);
